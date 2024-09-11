e5646e80aa3e61ac86d4b0ac9fa5762909aa6c86
Haci Kale <kale.hk@gmail.com>
2024-09-11 23:26:55 +0200

    added pagination

--- haci/src/components/app/App.tsx
+++ haci/src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import { useFetchChildren } from "../../global";
 import { DaycareChild } from "../daycareChild";
@@ -6,6 +6,22 @@ import { DaycareChild } from "../daycareChild";
 export function App() {
     const { fn: getChildren, data, error, loading } = useFetchChildren();
 
+    const [offset, setOffset] = useState(1);
+    const limit = 5;
+
+    const currentPageNumber = offset * limit - limit;
+    const paginatedChildren = data?.children.slice(currentPageNumber, currentPageNumber + limit);
+
+    const totalItems = data?.children.length;
+    const hasMorePages = totalItems ? currentPageNumber + limit < totalItems : false;
+    const hasPreviousPages = offset > 1;
+
+    const handlePrev = () => {
+        setOffset(offset - 1);
+    };
+    const handleNext = () => {
+        setOffset(offset + 1);
+    };
     useEffect(() => {
         getChildren();
     }, [getChildren]);
@@ -20,9 +36,17 @@ export function App() {
 
     return (
         <div>
-            {data?.children.map((child) => (
+            {paginatedChildren?.map((child) => (
                 <DaycareChild key={child.childId} child={child} />
             ))}
+            <div className='actions'>
+                <button disabled={!hasPreviousPages} onClick={handlePrev}>
+                    Previous
+                </button>
+                <button disabled={!hasMorePages} onClick={handleNext}>
+                    Next
+                </button>
+            </div>
         </div>
     );
 }
